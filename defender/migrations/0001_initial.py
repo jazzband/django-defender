345077725d2bbbce2709bc55f59d8cfef501ad61
Marcus Martins <marcus.martins@docker.com>
2015-01-05 18:40:29 -0800

    Add support for migrations under South and Django 1.7

---
+++ defender/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AccessAttempt',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user_agent', models.CharField(max_length=255)),
+                ('ip_address', models.GenericIPAddressField(null=True, verbose_name='IP Address')),
+                ('username', models.CharField(max_length=255, null=True)),
+                ('http_accept', models.CharField(max_length=1025, verbose_name='HTTP Accept')),
+                ('path_info', models.CharField(max_length=255, verbose_name='Path')),
+                ('attempt_time', models.DateTimeField(auto_now_add=True)),
+                ('login_valid', models.BooleanField(default=False)),
+            ],
+            options={
+                'ordering': ['-attempt_time'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
---
+++ defender/south_migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'AccessAttempt'
+        db.create_table(u'defender_accessattempt', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user_agent', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('ip_address', self.gf('django.db.models.fields.GenericIPAddressField')(max_length=39, null=True)),
+            ('username', self.gf('django.db.models.fields.CharField')(max_length=255, null=True)),
+            ('http_accept', self.gf('django.db.models.fields.CharField')(max_length=1025)),
+            ('path_info', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('attempt_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('login_valid', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'defender', ['AccessAttempt'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'AccessAttempt'
+        db.delete_table(u'defender_accessattempt')
+
+
+    models = {
+        u'defender.accessattempt': {
+            'Meta': {'ordering': "[u'-attempt_time']", 'object_name': 'AccessAttempt'},
+            'attempt_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'http_accept': ('django.db.models.fields.CharField', [], {'max_length': '1025'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39', 'null': 'True'}),
+            'login_valid': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'path_info': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'user_agent': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['defender']
