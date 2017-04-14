97cf0bd2a0efaca8c9a6ee5432d8128589e3fd33
Ken Cochrane <KenCochrane@gmail.com>
2017-04-14 16:24:08 -0400

    updated travis dependencies and fixed issue with 1.10,1.11 tests
    Signed-off-by: Ken Cochrane <KenCochrane@gmail.com>

--- defender/test_settings.py
+++ defender/test_settings.py
@@ -33,6 +33,17 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
 
