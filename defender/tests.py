c0534d83f8f514a4930490ed5989d9e789e35110
Israel Saeta Pérez <dukebody@gmail.com>
2017-06-10 00:04:16 +0200

    Allow usernames with plus signs in unblock view
    This fixes bug #GH76 where an exception like
    
    ```
    Reverse for 'defender_unblock_username_view' with arguments '(u'user+test@domain.tld',)' and keyword arguments '{}' not found. 1 pattern(s) tried: [u'admin/defender/blocks/username/(?P[A-Za-z0-9-._@]+)/unblock$']
    ```
    
    was raised when trying to access the `/admin/defender/blocks/` URL when a user with a plus sign had been locked out.

--- defender/tests.py
+++ defender/tests.py
@@ -532,6 +532,16 @@ class AccessAttemptTest(DefenderTestCase):
         from .admin import AccessAttemptAdmin
         AccessAttemptAdmin
 
+    def test_unblock_view_user_with_plus(self):
+        """
+        There is an available admin view for unblocking a user
+        with a plus sign in the username.
+
+        Regression test for #GH76.
+        """
+        reverse('defender_unblock_username_view',
+                kwargs={'username': 'user+test@test.tld'})
+
     def test_decorator_middleware(self):
         # because watch_login is called twice in this test (once by the
         # middleware and once by the decorator) we have half as many attempts
