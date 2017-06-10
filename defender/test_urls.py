69db1cfb7071f6dcf69e187c4ea38d6a63a6f557
Israel Saeta Pérez <dukebody@gmail.com>
2017-06-10 16:39:19 +0200

    Allow usernames with plus signs in unblock view (#77)
    This fixes bug #GH76 where an exception like
    
    ```
    Reverse for 'defender_unblock_username_view' with arguments '(u'user+test@domain.tld',)' and keyword arguments '{}' not found. 1 pattern(s) tried: [u'admin/defender/blocks/username/(?P[A-Za-z0-9-._@]+)/unblock$']
    ```
    
    was raised when trying to access the `/admin/defender/blocks/` URL when a user with a plus sign had been locked out.

--- defender/test_urls.py
+++ defender/test_urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from .urls import urlpatterns as original_urlpatterns
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+] + original_urlpatterns
--- defender/urls.py
+++ defender/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
         name="defender_blocks_view"),
     url(r'^blocks/ip/(?P<ip_address>[A-Za-z0-9-._]+)/unblock$', unblock_ip_view,
         name="defender_unblock_ip_view"),
-    url(r'^blocks/username/(?P<username>[A-Za-z0-9-._@]+)/unblock$',
+    url(r'^blocks/username/(?P<username>[A-Za-z0-9-._@\+]+)/unblock$',
         unblock_username_view,
         name="defender_unblock_username_view"),
 ]
