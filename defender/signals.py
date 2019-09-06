fcfa88d67905a9534a143f5ebeab35148593e534
horida <unknown>
2019-09-06 10:47:27 +0200

    Add unblock signals

--- defender/signals.py
+++ defender/signals.py
@@ -1,7 +1,10 @@
 from django.dispatch import Signal
 
 username_block = Signal(providing_args=['username'])
+username_unblock = Signal(providing_args=['username'])
 ip_block = Signal(providing_args=['ip_address'])
+ip_unblock = Signal(providing_args=['ip_address'])
+
 
 class BlockSignal:
     """
@@ -10,8 +13,18 @@ class BlockSignal:
     """
     pass
 
+
 def send_username_block_signal(username):
     username_block.send(sender=BlockSignal, username=username)
 
+
+def send_username_unblock_signal(username):
+    username_unblock.send(sender=BlockSignal, username=username)
+
+
 def send_ip_block_signal(ip_address):
     ip_block.send(sender=BlockSignal, ip_address=ip_address)
+
+
+def send_ip_unblock_signal(ip_address):
+    ip_unblock.send(sender=BlockSignal, ip_address=ip_address)
