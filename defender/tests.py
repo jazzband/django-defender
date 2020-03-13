71312eb841b3843838b3a135f46d61067b9a8602
calmkart <494296669@qq.com>
2020-03-13 20:13:54 +0800

    FIX: support for special character in redis password(such like '@') (#155)
    * FIX: if special character in redis password, we can set DEFENDER_REDIS_PASSWORD_QUOTE to True, and use quote password
    
    * MOD:add test cases with password_quota = True

--- defender/tests.py
+++ defender/tests.py
@@ -491,52 +491,67 @@ class AccessAttemptTest(DefenderTestCase):
     def test_parse_redis_url(self):
         """ test the parse_redis_url method """
         # full regular
-        conf = parse_redis_url("redis://user:password@localhost2:1234/2")
+        conf = parse_redis_url("redis://user:password@localhost2:1234/2", False)
         self.assertEqual(conf.get("HOST"), "localhost2")
         self.assertEqual(conf.get("DB"), 2)
         self.assertEqual(conf.get("PASSWORD"), "password")
         self.assertEqual(conf.get("PORT"), 1234)
 
         # full non local
-        conf = parse_redis_url("redis://user:pass@www.localhost.com:1234/2")
+        conf = parse_redis_url(
+            "redis://user:pass@www.localhost.com:1234/2", False)
         self.assertEqual(conf.get("HOST"), "www.localhost.com")
         self.assertEqual(conf.get("DB"), 2)
         self.assertEqual(conf.get("PASSWORD"), "pass")
         self.assertEqual(conf.get("PORT"), 1234)
 
         # no user name
-        conf = parse_redis_url("redis://password@localhost2:1234/2")
+        conf = parse_redis_url("redis://password@localhost2:1234/2", False)
         self.assertEqual(conf.get("HOST"), "localhost2")
         self.assertEqual(conf.get("DB"), 2)
         self.assertEqual(conf.get("PASSWORD"), None)
         self.assertEqual(conf.get("PORT"), 1234)
 
         # no user name 2 with colon
-        conf = parse_redis_url("redis://:password@localhost2:1234/2")
+        conf = parse_redis_url("redis://:password@localhost2:1234/2", False)
         self.assertEqual(conf.get("HOST"), "localhost2")
         self.assertEqual(conf.get("DB"), 2)
         self.assertEqual(conf.get("PASSWORD"), "password")
         self.assertEqual(conf.get("PORT"), 1234)
 
         # Empty
-        conf = parse_redis_url(None)
+        conf = parse_redis_url(None, False)
         self.assertEqual(conf.get("HOST"), "localhost")
         self.assertEqual(conf.get("DB"), 0)
         self.assertEqual(conf.get("PASSWORD"), None)
         self.assertEqual(conf.get("PORT"), 6379)
 
         # no db
-        conf = parse_redis_url("redis://:password@localhost2:1234")
+        conf = parse_redis_url("redis://:password@localhost2:1234", False)
         self.assertEqual(conf.get("HOST"), "localhost2")
         self.assertEqual(conf.get("DB"), 0)
         self.assertEqual(conf.get("PASSWORD"), "password")
         self.assertEqual(conf.get("PORT"), 1234)
 
         # no password
-        conf = parse_redis_url("redis://localhost2:1234/0")
+        conf = parse_redis_url("redis://localhost2:1234/0", False)
         self.assertEqual(conf.get("HOST"), "localhost2")
         self.assertEqual(conf.get("DB"), 0)
         self.assertEqual(conf.get("PASSWORD"), None)
+        self.assertEqual(conf.get("PORT"), 1234)
+
+        # password with special character and set the password_quote = True
+        conf = parse_redis_url("redis://:calmkart%23%40%21@localhost:6379/0", True)
+        self.assertEqual(conf.get("HOST"), "localhost")
+        self.assertEqual(conf.get("DB"), 0)
+        self.assertEqual(conf.get("PASSWORD"), "calmkart#@!")
+        self.assertEqual(conf.get("PORT"), 6379)
+
+        # password without special character and set the password_quote = True
+        conf = parse_redis_url("redis://:password@localhost2:1234", True)
+        self.assertEqual(conf.get("HOST"), "localhost2")
+        self.assertEqual(conf.get("DB"), 0)
+        self.assertEqual(conf.get("PASSWORD"), "password")
         self.assertEqual(conf.get("PORT"), 1234)
 
     @patch("defender.config.DEFENDER_REDIS_NAME", "default")
